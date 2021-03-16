b81f3199155d54b7195209331eff0ee91686ff69
Kawa AKimune <akimune1003@gmail.com>
2021-03-16 21:01:59 +0900

    解いた

--- ABC/ABC192/E.py
+++ ABC/ABC192/E.py
@@ -1,17 +1,38 @@
-n, m, x, y = map(int, input().split())
+from sys import stdin
+import heapq
 
-d = {}
-for _ in range(m):
-    a, b, t, k = map(int, input().split())
-    if a in d.keys():
-        d[a].append([b, t, k])
-    else:
-        d[a] = [[b, t, k]]
-    if b in d.keys():
-        d[b].append([a, t, k])
-    else:
-        d[b] = [[a, t, k]]
+def main():
+    input = stdin.readline
+    n, m, x, y = map(int, input().split())
+    x -= 1
+    y -= 1
 
-mins = [-1 for _ in range(n+1)]
-    
-mins[x] = 0
+    elist = [[] for _  in range(n)]
+    for _ in range(m):
+        a, b, t, k = map(int, input().split())
+        a -= 1
+        b -= 1
+        elist[a].append([b, t, k])
+        elist[b].append([a, t, k])
+
+    d = []
+    heapq.heapify(d)
+    d.append((0,x))
+    visited = [10**18]*n
+    visited[x] = 0
+
+    while len(d) > 0:
+        now, r = heapq.heappop(d)
+        if r == y:
+            print(now)
+            return
+        for i, t, k in elist[r]:
+            time = ((now-1)//k + 1)*k + t
+            if time < visited[i]:
+                heapq.heappush(d,(time,i))
+                visited[i] = time
+
+    print(-1)
+    return
+
+main()
